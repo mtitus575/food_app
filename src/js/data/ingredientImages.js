f9e3f6412384e82541704d92184ff75fb20b5ae1
Martin <mjtitus06@gmail.com>
2025-08-26 20:54:56 +0100

    Added a file to hold an array of objects for the individual ingredients' images. Deleted the test_localStorage.js file.

---
+++ src/js/data/ingredientImages.js
@@ -0,0 +1,214 @@
+export const ingredientImages = [
+  {
+    id: 1,
+    ingredientName: "onion",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/607856060e038e53063b2f0a-92a47b95.png",
+  },
+  {
+    id: 2,
+    ingredientName: "pak choi",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/62fcaa4ec9a48553f90753f6-de8af61e.png",
+  },
+  {
+    id: 3,
+    ingredientName: "smoked tofu",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/62fcaa3dbd9a2478e193f068-cda503d3.png",
+  },
+  {
+    id: 4,
+    ingredientName: "lime",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/60390c61b36c4838656814ba-76f1e4d9.png",
+  },
+  {
+    id: 5,
+    ingredientName: "yellow thai style paste",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/6762b71aafc243b64a9fbadf-6a1a2b57.png",
+  },
+  {
+    id: 6,
+    ingredientName: "coconut milk",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/62fcaa0abd9a2478e193f002-af8fb6ac.png",
+  },
+  {
+    id: 7,
+    ingredientName: "steamed brown basmati rice",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/62fcaa0dbd9a2478e193f006-79a834a5.png",
+  },
+  {
+    id: 8,
+    ingredientName: "baby spinach",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/62fcaa0ec9a48553f907536e-df5e925c.png",
+  },
+  {
+    id: 9,
+    ingredientName: "chilli flakes",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/62fcaa18bd9a2478e193f014-f0d9fce2.png",
+  },
+  {
+    id: 10,
+    ingredientName: "lentils",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/62fcaa28bd9a2478e193f036-14a1be3c.png",
+  },
+  {
+    id: 11,
+    ingredientName: "curry powder mix",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/62fcaa0ec9a48553f907536f-bd93057c.png",
+  },
+  {
+    id: 12,
+    ingredientName: "tomato puree",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/606335279cc1081ad772a604-399f5463.png",
+  },
+  {
+    id: 13,
+    ingredientName: "basa",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/63cfe0ac9ef03d3cc45329f9-71a5a954.png",
+  },
+  {
+    id: 14,
+    ingredientName: "water",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/62c6e0ef9cab2001267a28b8-96ef4bff.png",
+  },
+  {
+    id: 15,
+    ingredientName: "salt",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/62c6e0eb9cab2001267a28b5-38e77541.png",
+  },
+  {
+    id: 16,
+    ingredientName: "parsley",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/62fcaa51bd9a2478e193f0ae-d9df6048.png",
+  },
+  {
+    id: 17,
+    ingredientName: "red chilli",
+    image:
+      "https://media.greenchef.com/w_96,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/60390809b47bdb2f3e439e0d-e12f1631.png",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+  {
+    id: 14,
+    ingredientName: "",
+    image: "",
+  },
+];
