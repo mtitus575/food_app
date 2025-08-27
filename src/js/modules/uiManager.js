7b2a7e38f4eec911f53abd4f0d8d31d3628851bc
Martin <mjtitus06@gmail.com>
2025-08-27 02:53:00 +0100

    Created and appended elements to the individual recipe main card. It needs to be styles and populated with recipe info.

--- src/js/modules/uiManager.js
+++ src/js/modules/uiManager.js
@@ -2,10 +2,6 @@ import { DataManager } from "./dataManager.js";
 import { titleCase } from "../utilities/helperFunctions.js";
 
 const uiManager = {
-  createDOMElement: function (elementType) {
-    return document.createElement(elementType);
-  },
-
   loadTotalRecipes: function () {
     const recipeSavedTotal = DataManager.getInitialRecipes().length;
     const recipeTotal = document.getElementById("recipe_total");
@@ -41,37 +37,97 @@ const uiManager = {
       recipeCalories.innerText = `Calories: ${0}`; //The zero here is a placeholder. Actual calories/per portion needed here.
 
       //add an event listener to each card:
-      recipeCard.addEventListener('click', this.handleRecipeClick)
+      recipeCard.addEventListener(
+        "click",
+        uiEventHandlers.handleRecipeClick(recipe)
+      );
     }
   },
-  handleRecipeClick: function (event){
-    event.preventDefault()
+};
 
-    console.log(`The ${event.target.className} was clicked!`)// remove, testing only
+const uiEventHandlers = {
+  //event handler for clicking each recipe:
+  handleRecipeClick: function (recipe) {
+    /*  This creates a closure and gives the eventHanldler function that is being returned,
+        access to the recipe object that gets passed in.*/
+    return function (event) {
+      event.preventDefault();
+
+      console.log(`The ${recipe.name} was clicked!`); //Remove: testing only.
+
+      //Hide:
+      uiHelpers.hideAllRecipes();
+      //show:
+      uiHelpers.showClickedRecipe(recipe);
+    };
+  },
+};
+
+const uiHelpers = {
+  hideAllRecipes: function () {
+    const mainRecipeContainer = document.querySelector(".hero");
+    mainRecipeContainer.style.display = "none";
+  },
+  showClickedRecipe: function (recipe) {
+    const body = document.querySelector("body");
 
     //create and add a card to the page:
-        const recipeHero = document.createElement("section");
-        recipeHero.className = "recipeHero";
-        const body = document.querySelector("body");
-        body.append(recipeHero); //append the main card to the main html body.
+    const recipeHero = document.createElement("section");
+    recipeHero.className = "recipeHero";
+    body.append(recipeHero); //append the main card to the main html body.
 
-        //display contents for that recipe in that card:
-        // recipeHero.innerText = recipe.image;
+    // Create and append elements inside the main `recipeHero` container.
+    // image:
+    const recipeImg = document.createElement("img");
+    recipeImg.src = recipe.image;
+    recipeImg.className = "recipeMainImage";
+    //container for details:
+    const recipeDetails = document.createElement("div");
+    recipeDetails.className = "recipeMainDetails";
+    recipeHero.append(recipeImg, recipeDetails);
 
-  }
+    //Create elements and append inside the `recipeDetails` container:
+    const highlights = document.createElement("article");
+    highlights.className = "recipeMainHighlights";
+    const instructions = document.createElement("article");
+    instructions.className = "recipeMainInstruct";
+    const btnCtn = document.createElement("div");
+    btnCtn.className = "recipeBtnCtn";
+    recipeDetails.append(highlights, instructions);
+
+    //Create and append elements inside the `highlights` container:
+    const recipeTitle = document.createElement("h1");
+    recipeTitle.className = "recipeMainTitle";
+    recipeTitle.innerText = titleCase(recipe.name);
+
+    const nutritionInfo = document.createElement("ul");
+    nutritionInfo.className = "recipeNutritionalInfo";
+
+    const basicInfo = document.createElement("article");
+    basicInfo.className = "recipeBasicInfo";
+    highlights.append(recipeTitle, nutritionInfo, basicInfo);
+
+    //Create and append details to `basicInfo` container:
+    const servings = document.createElement('p')
+    servings.innerText = `Serves: ${recipe.servings}`
+    const cookTime = document.createElement('p')
+    cookTime.innerText = `Cooking Time: ${recipe.cookTime} minutes`
+    basicInfo.append(servings, cookTime)
+
+    //Create and append details to the `instructions` container
+    const instructionItems = document.createElement("ol");
+    instructionItems.className = "recipeMainInstructions";
+    instructions.append(instructionItems);
+
+    //Create and append buttons in the `btnCtn`
+    const cookedBtn = document.createElement("button");
+    cookedBtn.className = "btn";
+    cookedBtn.innerText = "Mark as Cooked";
+    const nextBtn = document.createElement("button");
+    nextBtn.className = "btn";
+    nextBtn.innerText = "Next Recipe";
+    btnCtn.append(cookedBtn, nextBtn);
+  },
 };
 
 export { uiManager };
-
-/*
-//supposed to make each recipe appear as a whole page.
-recipeCard.addEventListener("click", function displayRecipe() {
-        //create and add a card to the page:
-        const recipeHero = document.createElement("section");
-        recipeHero.className = "recipeHero";
-        body.append(recipeHero); //append the main card to the main html body.
-
-        //display contents for that recipe in that card:
-        recipeHero.innerText = recipe.image;
-      });
-*/
